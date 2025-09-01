325e4006ef5d65359e4b80313d477f71e9233be7
RodrigoQF <rodrigo.defrancischi@gmail.com>
2025-09-01 10:44:49 -0300

    Criacao Vacina model

--- Model.cs
+++ Model.cs
@@ -6,3 +6,12 @@ public record Pessoa
     public int Idade { get; set; }
     public string Sexo { get; set; } = default!;
 }
+
+public record Vacina
+{
+    public string Id { get; init; } = default!;
+    public string Nome { get; set; } = default!;
+    public string Data { get; set; } = default!;   
+    public string Dose { get; set; } = default!;
+    public string Fabricante { get; set; } = default!;
+}
